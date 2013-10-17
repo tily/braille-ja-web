8571a0f45c1e6ed6e3e59d4a5ce9f8bc88776a30
Tadashi Saito <tad.a.digger@gmail.com>
2013-10-17 23:01:17 +0900

    allow access from tadd.github.{com,io}.

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ get '/translate' do
   text = params[:text]
   error 400, {error: 'text required'}.to_json if text.nil?
   error 400, {error: 'text too long'}.to_json if text.length > 100
+
+  headers "Access-Control-Allow-Origin" => "tadd.github.io tadd.github.com"
   {translated: text.kana_to_braille}.to_json
 end
 

903f24f8a122f2ceb784b53383bfe6552aed445e
Tadashi Saito <tad.a.digger@gmail.com>
2013-10-18 23:22:50 +0900

    use cache_control() and etag(). finally I got 304!.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require 'json'
 require 'braille-ja'
+require 'digest'
 
 get '/translate' do
   content_type :json
@@ -9,8 +10,9 @@ get '/translate' do
   error 400, {error: 'text required'}.to_json if text.nil?
   error 400, {error: 'text too long'}.to_json if text.length > 100
 
-  headers "Access-Control-Allow-Origin" => 'http://tadd.github.io',
-    'Cache-Control' => 'public, max-age=3600'
+  cache_control :public, max_age: 3600
+  headers "Access-Control-Allow-Origin" => 'http://tadd.github.io'
+  etag Digest::SHA1.hexdigest(text)
   {translated: text.kana_to_braille}.to_json
 end
 

0f40c7b0b98c4247ac0487c4a415109c487600fa
Tadashi Saito <tad.a.digger@gmail.com>
2013-10-18 23:44:25 +0900

    define helpers in the passed block for helpers().

--- app.rb
+++ app.rb
@@ -5,13 +5,15 @@ require 'braille-ja'
 require 'digest'
 require 'dalli'
 
-def setup_cache(seed)
-  cache_control :public, max_age: 3600
-  etag Digest::SHA1.hexdigest(seed)
-  if memcachier_servers = ENV["MEMCACHIER_SERVERS"]
-    set :cache, Dalli::Client.new(memcachier_servers,
-                                  username: ENV["MEMCACHIER_USERNAME"],
-                                  password: ENV["MEMCACHIER_PASSWORD"])
+helpers do
+  def setup_cache(seed)
+    cache_control :public, max_age: 3600
+    etag Digest::SHA1.hexdigest(seed)
+    if memcachier_servers = ENV["MEMCACHIER_SERVERS"]
+      set :cache, Dalli::Client.new(memcachier_servers,
+                                    username: ENV["MEMCACHIER_USERNAME"],
+                                    password: ENV["MEMCACHIER_PASSWORD"])
+    end
   end
 end
 

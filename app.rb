07fa078ae2ea6daa94499a3c7f0b903ffc9f3b3b
Tadashi Saito <tad.a.digger@gmail.com>
2013-10-18 23:32:23 +0900

    refactoring.

--- app.rb
+++ app.rb
@@ -4,15 +4,19 @@ require 'json'
 require 'braille-ja'
 require 'digest'
 
+def setup_cache(seed)
+  cache_control :public, max_age: 3600
+  etag Digest::SHA1.hexdigest(seed)
+end
+
 get '/translate' do
   content_type :json
   text = params[:text]
   error 400, {error: 'text required'}.to_json if text.nil?
   error 400, {error: 'text too long'}.to_json if text.length > 100
 
-  cache_control :public, max_age: 3600
   headers "Access-Control-Allow-Origin" => 'http://tadd.github.io'
-  etag Digest::SHA1.hexdigest(text)
+  setup_cache(text)
   {translated: text.kana_to_braille}.to_json
 end
 

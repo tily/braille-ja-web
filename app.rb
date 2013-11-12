c3ba66f12c48fea06ac977a643531c9a3bcd21b7
Tadashi Saito <tad.a.digger@gmail.com>
2013-11-12 12:04:56 +0900

    access from any origin.

--- app.rb
+++ app.rb
@@ -26,14 +26,14 @@ get '/translate' do
   error 400, {error: 'text required'}.to_json if text.nil?
   error 400, {error: 'text too long'}.to_json if text.length > 100
 
-  headers "Access-Control-Allow-Origin" => 'http://tadd.github.io'
+  headers "Access-Control-Allow-Origin" => '*'
   setup_cache(text)
   {translated: text.to_braille}.to_json
 end
 
 get '/ping' do
   content_type :txt
-  headers "Access-Control-Allow-Origin" => 'http://tadd.github.io'
+  headers "Access-Control-Allow-Origin" => '*'
   'pong'
 end
 
